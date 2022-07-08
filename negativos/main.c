cf5dfb288605b1f93395b492e37a768f63b57d0e
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-08 00:25:20 -0300

    negativos

---
+++ negativos/main.c
@@ -0,0 +1,33 @@
+//Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois N números inteiros
+//e armazene-os em um vetor. Em seguida, mostrar na tela todos os números negativos lidos.
+
+#include <stdio.h>
+
+int main()
+{
+
+    int N;
+
+    printf("Quantos numeros voce vai digitar? ");
+    scanf("%d", &N);
+
+    int vet[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &vet[i]);
+    }
+
+    printf("\nNUMEROS NEGATIVOS:\n");
+
+    for (int i = 0; i < N; i++)
+    {
+        if (vet[i] < 0)
+        {
+            printf("%d\n", vet[i]);
+        }
+    }
+
+    return 0;
+}
