2b9c2658c079e9bc051cf174af1fed9e690d56ca
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-07 23:35:34 -0300

    menor de tress

---
+++ menor_de_tres/main.c
@@ -0,0 +1,33 @@
+//Fazer um programa para ler três números inteiros. Em seguida, mostrar qual o menor dentre os três
+//números lidos. Em caso de empate, mostrar apenas uma vez.
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b, c, menor;
+
+    printf("Primeiro valor: ");
+    scanf("%d", &a);
+
+    printf("Segundo valor: ");
+    scanf("%d", &b);
+
+    printf("Terceiro valor: ");
+    scanf("%d", &c);
+
+    if (a < b && a < c) {
+        menor = a;
+    }
+    else if (b < c) {
+        menor = b;
+    }
+    else {
+        menor = c;
+    }
+
+    printf("MENOR = %d\n", menor);
+
+    return 0;
+}
